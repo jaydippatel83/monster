4141a2d135681b715c08ca9729d743cf382cc98e
Karan Pujara <karanjpujara@gmail.com>
2019-12-27 13:17:47 +0530

    Updates

---
+++ precache-manifest.de2f325fccc6394bc317c2fee8c0ce4b.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0cffdd4b611a54a43a19ef76f325d40a",
+    "url": "/monster/index.html"
+  },
+  {
+    "revision": "468776dba7a54c804e06",
+    "url": "/monster/static/css/main.c9f2d356.chunk.css"
+  },
+  {
+    "revision": "4b54a7d42f1f0a9a6ce5",
+    "url": "/monster/static/js/2.35db5147.chunk.js"
+  },
+  {
+    "revision": "468776dba7a54c804e06",
+    "url": "/monster/static/js/main.5b888146.chunk.js"
+  },
+  {
+    "revision": "844d49f6b6ecc47bb440",
+    "url": "/monster/static/js/runtime-main.28df6bfc.js"
+  }
+]);
